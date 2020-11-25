37e413b87c4c4d209b9a685a200947f8c3b62692
vinezi <wewewe.ww52@gmail.com>
2020-11-25 17:03:09 +0300

    1 commit

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary2
+{
+    public class helper
+    {
+        private string value1;
+        private string value2;
+        public helper(string value1, string value2)
+        {
+            this.value1 = value1;
+            this.value2 = value2;
+        }
+        public string GetLog()
+        {
+            return "" + Math.Log(Double.Parse(value1), Double.Parse(value2));
+        }
+        public string GetClear()
+        {
+            return "";
+        }
+        public bool IsEmpty()
+        {
+            bool Flag = false;
+            if (value1.Length == 0 || value2.Length == 0)
+            {
+                Flag = true;
+            }
+            return Flag;
+        }
+
+
+    }
+}
